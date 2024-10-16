f0946f710ebd15cea1d6a53499a9ff2682078da9
Anuj Sinha <sinha.anuj30@gmail.com>
2024-10-16 16:32:24 -0700

    style: update dummy test

--- src/support_sphere/test/unit_tests/dummy_test.dart
+++ src/support_sphere/test/unit_tests/dummy_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Counter value should be incremented', () {
+  test('This is just a dummy unit test', () {
 
     expect(1, 1);
   });
