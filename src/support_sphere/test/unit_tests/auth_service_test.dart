a3436ca0dec00c17bf913590a4686b49424728aa
Anuj Sinha <sinha.anuj30@gmail.com>
2024-10-17 15:18:36 -0700

    docs: add description for test group and setup

--- src/support_sphere/test/unit_tests/auth_service_test.dart
+++ src/support_sphere/test/unit_tests/auth_service_test.dart
@@ -6,11 +6,17 @@ void main() {
 
   late AuthService authService;
 
+  // This is the parent level setUp() function that runs before each test defined in this class.
   setUp(() {
     authService = AuthService();
   });
 
-  group('AuthService', () {
+  // A group is used to scope together tests having similar description, and
+  // group level setUp() or tearDown() functions.
+  // This should not be confused with the parent level setUp() and tearDown()
+  // Refer: https://docs.flutter.dev/cookbook/testing/unit/introduction#5-combine-multiple-tests-in-a-group
+
+  group('AuthService SignUp Code Validation Tests', () {
     test('isSignupCodeValid returns true for valid code', () async {
       final isValid = await authService.isSignupCodeValid('SUPPORT');
       expect(isValid, isTrue);
